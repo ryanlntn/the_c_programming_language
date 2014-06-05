be50bfe32e14faa75c7e3c464910f4a41bac478f
Ryan Linton <ryanlntn@gmail.com>
2014-06-05 05:27:06 -0700

    Add heading

--- fahr-celsius.c
+++ fahr-celsius.c
@@ -4,17 +4,18 @@
  *  for fahr = 0, 20, ..., 300 */
 main()
 {
-  int fahr, celsius;
-  int lower, upper, step;
+  float fahr, celsius;
+  float lower, upper, step;
 
   lower = 0;       /* lower limit of temperature scale */
   upper = 300;     /* upper limit */
   step = 20;       /* step size */
 
   fahr = lower;
+  printf("Fahrenheit-Celsius Conversion Table\n");
   while (fahr <= upper) {
-      celsius = 5 * (fahr-32) / 9;
-      printf("%3d\t%6d\n", fahr, celsius);
+      celsius = (5.0/9.0) * (fahr-32.0);
+      printf("%3.0f\t%6.1f\n", fahr, celsius);
       fahr = fahr + step;
   }
 }
