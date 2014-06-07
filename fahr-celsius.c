10abdbf814f6a2d6804cb607557dfcb4ae3284b0
Ryan Linton <ryanlntn@gmail.com>
2014-06-06 20:19:29 -0700

    Use constants

--- fahr-celsius.c
+++ fahr-celsius.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table
- *  for fahr = 0, 20, ..., 300 */
+#define LOWER  0
+#define UPPER  300
+#define STEP   20
+
+/* print Fahrenheit-Celsius table */
 main()
 {
     int fahr;
 
-    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
