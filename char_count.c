a7c83a75cdeea8a9adee1d80369d27bc1f6ec81f
Ryan Linton <ryanlntn@gmail.com>
2014-06-08 04:36:09 -0700

    Modify char count to use double and for loop

--- char_count.c
+++ char_count.c
@@ -3,10 +3,9 @@
 /* count characters in input; 1st version */
 main()
 {
-    long nc;
+    double nc;
 
-    nc = 0;
-    while (getchar() != EOF)
-        ++nc;
-    printf("%ld\n", nc);
+    for (nc = 0; getchar() != EOF; ++nc)
+      ;
+    printf("%.0f\n", nc);
 }
